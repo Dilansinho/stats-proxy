d05850ba447eb250ec5ea5812bc6e1e016f7c268
Dilansinho <167827725+Dilansinho@users.noreply.github.com>
2026-04-18 21:49:39 -0500

    Create football.js

---
+++ api/football.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-apisports-key');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const { path, token } = req.query;
+
+  if (!path || !token) {
+    return res.status(400).json({ error: 'Faltan path y token' });
+  }
+
+  try {
+    const url = 'https://v3.football.api-sports.io/' + path;
+    const response = await fetch(url, {
+      headers: { 'x-apisports-key': token }
+    });
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
